bc2ccf48f87f99cfa2817df70c493ca13e6b5bf2
Taewoo Kim <taewookim0927@gmail.com>
2024-11-16 18:28:12 -0800

    <Foundation files> 1. main.c : testing with blinking LED 2. json files : tasks to use run commands, updaed include linker script paths. 3. Makefile : to intergrate commands that will be called from tasks.json

---
+++ Src/main.c
@@ -0,0 +1,20 @@
+#include <msp430.h>
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    volatile unsigned int i;
+	WDTCTL = WDTPW | WDTHOLD;	                // stop watchdog timer
+    P1DIR |= 0x01;                              // Set P1.0 to output direction
+
+    while(1)
+    {
+        P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+
+        for (i=500000; i>0; i--);
+    }
+}
+
