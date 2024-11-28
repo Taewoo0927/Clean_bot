0d4b44e3378c6c253178a482ccacc127bbe3fc78
Taewoo0927 <taewookim0927@gmail.com>
2024-11-28 12:02:32 -0800

    <io.h> 1. commiting test

--- Src/main.c
+++ Src/main.c
@@ -1,21 +1,21 @@
 #include <msp430.h>
 #include "Drivers/io.h"
 
-//For checking
+// For checking
 /**
  * main.c
  */
 int main(void)
 {
     volatile unsigned int i;
-	WDTCTL = WDTPW | WDTHOLD;	                // stop watchdog timer
-    P1DIR |= 0x01;                              // Set P1.0 to output direction
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+    P1DIR |= 0x01;            // Set P1.0 to output direction
 
-    while(1)
+    while (1)
     {
-        P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+        P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
 
-        for (i=1000000; i>0; i--);
+        for (i = 10000; i > 0; i--)
+            ;
     }
 }
-
