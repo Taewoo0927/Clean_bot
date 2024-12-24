d7e793cc8fd2cae6d8e1bf9a4949320167b30135
Taewoo0927 <taewookim0927@gmail.com>
2024-12-24 00:43:43 -0800

    <io.h, io.c> 1. Created io_set_sel function to choose which mode I want to use pin as. Also used Lookup table to optimize the function
    2. Organized reference folder

--- Src/Drivers/io.c
+++ Src/Drivers/io.c
@@ -1,15 +1,41 @@
+#include <msp430.h>
 #include "Drivers/io.h"
 
-/* Creating mask to fine port */
-#define IO_PORT_OFFSET (3u)
-#define IO_PORT_MASK (0x3u << IO_PORT_OFFSET)
+/* Creating mask to find port */
+#define IO_PORT(io) ((io) / 10)               // Extract Port number
+#define IO_PIN_IDX(io) (io % 10)              // Extract Pin Index
+#define IO_PINT_BIT(io) (1 << IO_PIN_IDX(io)) // Shift to compute Pin BIT
+
+// Create Look Up table for optimizing Port1 & Port2 select in switch statement
+static volatile unsigned char *PXSEL1[2] = {&P1SEL, &P2SEL};
+static volatile unsigned char *PXSEL2[2] = {&P1SEL2, &P2SEL2};
 
 void io_set_sel(io_e io, io_sel_e sel)
 {
+    unsigned int port = IO_PORT(io);        // Extract Port number
+    unsigned int pin_bit = IO_PINT_BIT(io); // Extract Pin Bit
+
+    // Switch statement to set the sel register
     switch (sel)
     {
     case IO_SEL_GPIO:
-        // Not decided yet
+        *PXSEL1[port] &= ~pin_bit;
+        *PXSEL2[port] &= ~pin_bit;
+        break;
+
+    case IO_SEL_ALT1:
+        *PXSEL1[port] |= pin_bit;
+        *PXSEL2[port] &= ~pin_bit;
+        break;
+
+    case IO_SEL_ALT2:
+        *PXSEL1[port] &= ~pin_bit;
+        *PXSEL2[port] |= pin_bit;
+        break;
+
+    case IO_SEL_ALT3:
+        *PXSEL1[port] |= pin_bit;
+        *PXSEL2[port] |= pin_bit;
         break;
     }
 }
