715171055908879ead0f608301d7986bfd01b45e
Taewoo0927 <taewookim0927@gmail.com>
2024-12-24 03:13:15 -0800

    <io.c> 1. Fixed #define IO_PORT(io) by subtraction 1, it is due to Lookuptable array indexing. 0 corresponds to P1 and 1 corresponds to P2 so after extracting port number it need to be subtracted by 1

--- Src/main.c
+++ Src/main.c
@@ -1,31 +1,22 @@
 #include <msp430.h>
-#include <stdint.h>
 #include "Drivers/io.h"
 
-#define IO_PORT(io) (((io) / 10) - 1)        // Extract Port number
-#define IO_PIN_IDX(io) (((io) % 10))         // Extract Pin Index
-#define IO_PIN_BIT(io) (1 << IO_PIN_IDX(io)) // Shift to compute Pin BIT
-
-static volatile unsigned char *const PXDIR[2] = {&P1DIR, &P2DIR};
-static volatile unsigned char *const PXOUT[2] = {&P1OUT, &P2OUT};
+// For checking
+/**
+ * main.c
+ */
 
 int main(void)
 {
     volatile unsigned int i;
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
 
-    // Test P1.0 (IO = 10)
-    unsigned int io = 10;                  // Port 1, Pin 0
-    unsigned int port = IO_PORT(io);       // Extract Port index
-    unsigned int pin_bit = IO_PIN_BIT(io); // Extract Pin bitmask
-
-    // Set P1.0 as output
-    *PXDIR[port] |= pin_bit; // Set P1.0 as output
+    io_set_sel(IO_TEST_LED, IO_SEL_GPIO);
+    io_set_dir(IO_TEST_LED, IO_DIR_OP);
 
     while (1)
     {
-        *PXOUT[port] ^= pin_bit; // Toggle P1.0
-        // P1OUT |= 0X01;
+        io_set_output(IO_TEST_LED, IO_PULL_STATE_HIGH);
 
         for (i = 10000; i > 0; i--)
             ;
