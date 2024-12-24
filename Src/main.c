0af84e0d9fabea0452c112a430109370a918f46c
Taewoo0927 <taewookim0927@gmail.com>
2024-12-24 02:44:44 -0800

    <main.c> 1. Debugging to check if io.c is working or not

--- Src/main.c
+++ Src/main.c
@@ -12,14 +12,15 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
 
     io_set_sel(IO_TEST_LED, IO_SEL_GPIO);
-    // io_set_dir(IO_TEST_LED, IO_DIR_OP);
+    io_set_dir(IO_TEST_LED, IO_DIR_OP);
 
-    P1DIR |= 0X01;
+    // P1DIR |= 0X01;
+    P1OUT &= ~0X01;
 
     while (1)
     {
-        io_set_output(IO_TEST_LED, IO_PULL_STATE_HIGH);
-        // P1OUT |= 0X01;
+        // io_set_output(IO_TEST_LED, IO_PULL_STATE_HIGH);
+        P1OUT |= 0X01;
 
         for (i = 10000; i > 0; i--)
             ;
