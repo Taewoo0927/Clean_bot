f1e29a69cd451304c2c5477275dea2bf854db607
Taewoo0927 <taewookim0927@gmail.com>
2024-12-24 02:33:07 -0800

    Auto-commit from Makefile

--- Src/main.c
+++ Src/main.c
@@ -5,33 +5,23 @@
 /**
  * main.c
  */
-static void blink_led(void)
-{
-    const struct io_configuration led_config =
-        {
-            .select = IO_SEL_GPIO,
-            .direction = IO_DIR_OP,
-            .resistor = IO_RESISTOR_DISABLED,
-            .output = IO_PULL_STATE_LOW};
-    io_configuration(IO_TEST_LED, &led_config);
-    io_out_e op_state = IO_PULL_STATE_LOW;
-
-    volatile unsigned int i = 1;
-
-    while (1)
-    {
-        // Toggle output state
-        op_state = (op_state == IO_PULL_STATE_LOW) ? IO_PULL_STATE_HIGH : IO_PULL_STATE_LOW;
-        // Set output of LED as out
-        io_set_output(IO_TEST_LED, op_state);
-        for (i = 10000; i > 0; i--)
-            ;
-    }
-}
 
 int main(void)
 {
+    volatile unsigned int i;
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
-    blink_led();
-    return 0;
+
+    io_set_sel(IO_TEST_LED, IO_SEL_GPIO);
+    io_set_dir(IO_TEST_LED, IO_DIR_OP);
+
+    io_out_e output = IO_PULL_STATE_LOW;
+
+    while (1)
+    {
+        io_set_output(IO_TEST_LED, IO_PULL_STATE_LOW);
+
+        for (i = 10000; i > 0; i--)
+            ;
+        io_set_output(IO_TEST_LED, IO_PULL_STATE_HIGH);
+    }
 }
