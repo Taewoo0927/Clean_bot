8d4e5093d7b8dc23b06e05ff6c182767507830a9
Taewoo0927 <taewookim0927@gmail.com>
2024-12-24 02:29:42 -0800

    Auto-commit from Makefile

--- Src/main.c
+++ Src/main.c
@@ -16,19 +16,21 @@ static void blink_led(void)
     io_configuration(IO_TEST_LED, &led_config);
     io_out_e op_state = IO_PULL_STATE_LOW;
 
+    volatile unsigned int i = 1;
+
     while (1)
     {
         // Toggle output state
         op_state = (op_state == IO_PULL_STATE_LOW) ? IO_PULL_STATE_HIGH : IO_PULL_STATE_LOW;
         // Set output of LED as out
         io_set_output(IO_TEST_LED, op_state);
-        __delay_cycles(250000);
+        for (i = 10000; i > 0; i--)
+            ;
     }
 }
 
 int main(void)
 {
-    volatile unsigned int i;
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
     blink_led();
     return 0;
