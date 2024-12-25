ade77c50a2d47153e9ed694d45fa9a6f96a59a13
Taewoo0927 <taewookim0927@gmail.com>
2024-12-25 00:22:11 -0800

    <main.c, io.c> 1. added structure for configuring the pins 2. tested on main.c by static function

--- Src/main.c
+++ Src/main.c
@@ -5,20 +5,31 @@
 /**
  * main.c
  */
+static void blink_led(void)
+{
+    volatile unsigned int i;
+    const struct io_configuration test_led = {
+        .direction = IO_DIR_OP,
+        .output = IO_PULL_STATE_LOW,
+        .resistor = IO_RESISTOR_DISABLED,
+        .select = IO_SEL_GPIO};
+
+    io_configuration(IO_TEST_LED, &test_led);
+    io_out_e output_state = IO_PULL_STATE_LOW;
+
+    while (1)
+    {
+        // Toggle output state
+        output_state = (output_state == IO_PULL_STATE_LOW ? IO_PULL_STATE_HIGH : IO_PULL_STATE_LOW);
+        io_set_output(IO_TEST_LED, output_state);
+        for (i = 10000; i > 0; i--)
+            ;
+    }
+}
 
 int main(void)
 {
     volatile unsigned int i;
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
-
-    io_set_sel(IO_TEST_LED, IO_SEL_GPIO);
-    io_set_dir(IO_TEST_LED, IO_DIR_OP);
-
-    while (1)
-    {
-        io_set_output(IO_TEST_LED, IO_PULL_STATE_HIGH);
-
-        for (i = 10000; i > 0; i--)
-            ;
-    }
+    blink_led();
 }
