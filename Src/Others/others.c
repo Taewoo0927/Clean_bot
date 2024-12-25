9e54a7e4579ab120dbcf86cc39404f906ac939a9
Taewoo0927 <taewookim0927@gmail.com>
2024-12-25 03:13:59 -0800

    <system_init.c> 1. encapsulated watchdog to make main function more readable

--- Src/Others/others.c
+++ Src/Others/others.c
@@ -4,10 +4,9 @@
 
 void _delay_ms(uint16_t miliseconds)
 {
-    uint8_t _1ms;
     while (miliseconds-- > 0)
     {
-        _1ms = DELAY_ITERATIONS_PER_MS;
+        uint8_t _1ms = DELAY_ITERATIONS_PER_MS;
         while (_1ms-- > 0)
             ;
     }
--- include/Others/others.h
+++ include/Others/others.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+/* Some useful functions that I'll be using on the main.c or anywhere else */
+
 void _delay_ms(uint16_t miliseconds);
 
 #endif // OTHERS_H
